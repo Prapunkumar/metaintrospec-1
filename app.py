a8e947d09b7ff74bf427b338b90bc8d3a3f171de
Prapunkumar <kumar.illapani@gmail.com>
2023-08-06 22:01:18 -0400

    updated redirect_uri

--- app.py
+++ app.py
@@ -25,7 +25,7 @@ client_secrets_file = os.path.join(pathlib.Path(__file__).parent,"client-secret.
 flow = Flow.from_client_secrets_file(
     client_secrets_file=client_secrets_file,
     scopes=["https://www.googleapis.com/auth/userinfo.profile","https://www.googleapis.com/auth/userinfo.email","openid"],
-    redirect_uri="http://127.0.0.1:5000/submit"
+    redirect_uri="metaintrospec-2.vercel.app"
     )
 openai.api_key = os.getenv("OPENAI_API_KEY")
 
