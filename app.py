c86cdc571c4545b6471ed7e2cf73b0d03571efa2
Prapunkumar <kumar.illapani@gmail.com>
2023-08-06 21:52:25 -0400

    only details

--- app.py
+++ app.py
@@ -42,13 +42,13 @@ Here are the questions and answers below:
 with open('questions.txt') as f:
   questions = f.readlines()
 # Set up the database connection
-def create_conn():
-    return mysql.connector.connect(
-        host="127.0.0.1",
-        user="root",
-        password="Priyakavya@8280",
-        database="students"
-    )
+# def create_conn():
+#     return mysql.connector.connect(
+#         host="127.0.0.1",
+#         user="root",
+#         password="Priyakavya@8280",
+#         database="students"
+#     )
 
 @app.route('/', methods=['GET', 'POST'])
 def details():
@@ -82,51 +82,51 @@ def submit_details():
 
     return render_template("details.html")
 
-@app.route('/save', methods=['POST'])
-def save_details():
+# @app.route('/save', methods=['POST'])
+# def save_details():
 
-    if request.method == 'POST':
-        # Process form submission
-        name = session["name"]
-        email = session["email"]
-        gender = request.form['gender'] 
+#     if request.method == 'POST':
+#         # Process form submission
+#         name = session["name"]
+#         email = session["email"]
+#         gender = request.form['gender'] 
 
-        conn = create_conn()
-        cursor = conn.cursor()
+#         conn = create_conn()
+#         cursor = conn.cursor()
 
-        query = "SELECT attempt_count FROM student_details WHERE email = %s"
+#         query = "SELECT attempt_count FROM student_details WHERE email = %s"
         
-        cursor.execute(query, (email,))
-        attempt_count= cursor.fetchone()
+#         cursor.execute(query, (email,))
+#         attempt_count= cursor.fetchone()
 
-        if attempt_count is None:
-            # Insert the new record if the email is not present
-            insert_query = "INSERT INTO student_details (name, email, gender,attempt_count) VALUES (%s,%s, %s, %s)"
-            cursor.execute(insert_query, (name, email,gender, 0))
-            conn.commit()
-            cursor.close()
-            conn.close()
-            return render_template('index.html', questions=questions[0:5],email=email,count=0,name=name)
-        else:
-            index=attempt_count[0]*5
+#         if attempt_count is None:
+#             # Insert the new record if the email is not present
+#             insert_query = "INSERT INTO student_details (name, email, gender,attempt_count) VALUES (%s,%s, %s, %s)"
+#             cursor.execute(insert_query, (name, email,gender, 0))
+#             conn.commit()
+#             cursor.close()
+#             conn.close()
+#             return render_template('index.html', questions=questions[0:5],email=email,count=0,name=name)
+#         else:
+#             index=attempt_count[0]*5
 
-            return render_template('index.html', questions=questions[index:index+5],email=email,count=attempt_count[0],name=name)
+#             return render_template('index.html', questions=questions[index:index+5],email=email,count=attempt_count[0],name=name)
         
-    return render_template('details.html')
+#     return render_template('details.html')
 
 
-@app.route('/index', methods=['POST'])
-def index():
+# @app.route('/index', methods=['POST'])
+# def index():
    
-    if request.method == 'POST':
-        name=request.form.get('name')
-        email = request.form.get('email')
-        questions=request.form.get('questions')
+#     if request.method == 'POST':
+#         name=request.form.get('name')
+#         email = request.form.get('email')
+#         questions=request.form.get('questions')
 
-        attempt_count= validate_user(email)
+#         attempt_count= validate_user(email)
 
-        answers = {f'answer{i}': request.form[f'answer{i}'] for i in range(attempt_count*5+1, attempt_count*5+6)}
-        answers_json = json.dumps(answers)
+#         answers = {f'answer{i}': request.form[f'answer{i}'] for i in range(attempt_count*5+1, attempt_count*5+6)}
+#         answers_json = json.dumps(answers)
 
 
         # message= get_scores(questions,answers)
@@ -151,46 +151,46 @@ def index():
 
         # # Output the average score
         # print("Average score:", average_score)
-        average_score=98.9
-        conn = create_conn()
-        cursor = conn.cursor()
+        # average_score=98.9
+        # conn = create_conn()
+        # cursor = conn.cursor()
 
-        query = "INSERT INTO student_eval (email, answers, score) VALUES (%s, %s,%s)"
-        cursor.execute(query, ( email, answers_json,int(average_score)))
+        # query = "INSERT INTO student_eval (email, answers, score) VALUES (%s, %s,%s)"
+        # cursor.execute(query, ( email, answers_json,int(average_score)))
 
-        conn.commit()
+        # conn.commit()
 
-        update_query = "UPDATE student_details SET attempt_count = attempt_count + 1 WHERE email = %s"
-        cursor.execute(update_query, (email,))
-        conn.commit()
-        cursor.close()
-        conn.close()
+        # update_query = "UPDATE student_details SET attempt_count = attempt_count + 1 WHERE email = %s"
+        # cursor.execute(update_query, (email,))
+        # conn.commit()
+        # cursor.close()
+        # conn.close()
     
 
-        return render_template('score.html',score=average_score,name= name)
-    return render_template('index.html', questions=questions)
+    #     return render_template('score.html',score=average_score,name= name)
+    # return render_template('index.html', questions=questions)
 
 
-def validate_user(email):
-    conn = create_conn()
-    cursor = conn.cursor()
-    query = "SELECT attempt_count FROM student_details WHERE email = %s"
-    cursor.execute(query, (email,))
-    attempt_count = cursor.fetchone()[0]
-    cursor.close()
-    conn.close()
-    return attempt_count
+# def validate_user(email):
+#     conn = create_conn()
+#     cursor = conn.cursor()
+#     query = "SELECT attempt_count FROM student_details WHERE email = %s"
+#     cursor.execute(query, (email,))
+#     attempt_count = cursor.fetchone()[0]
+#     cursor.close()
+#     conn.close()
+#     return attempt_count
 
-def get_scores(questions,answers):
-    response= openai.ChatCompletion.create(
-    model="gpt-3.5-turbo",
-    messages=[
-        {"role":"user","content":prompt1 + "Questions" +  questions + "Answers"+ str(answers)}
-    ],
-    max_tokens=100,  
-    )
-    print(response)
-    return response
+# def get_scores(questions,answers):
+#     response= openai.ChatCompletion.create(
+#     model="gpt-3.5-turbo",
+#     messages=[
+#         {"role":"user","content":prompt1 + "Questions" +  questions + "Answers"+ str(answers)}
+#     ],
+#     max_tokens=100,  
+#     )
+#     print(response)
+#     return response
 
 if __name__ == '__main__':
   app.run(debug=True)
